1d7eb6d7f2c1e8c3ae38c7b0e13db9cd6a727a23
KulthumRadi <kulthumradi94@gmail.com>
2022-05-30 19:45:08 +0300

    cham=nging on db and app js continue

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 require("./db/conn");
+const User = require('./models/User')
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
@@ -15,3 +16,62 @@ app.get("/", (req, res) => {
 app.get("/admin", (req, res) => {
     res.sendFile(path.join(__dirname, 'Admin/Admin.html'));
 })
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, 'Login/Login.html'));
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, 'Register/Register.html'));
+})
+
+app.post("/userdata", async (req, res) => {
+    try {
+        TransationAmount = req.body.TransationAmount
+        TransationTitle = req.body.TransationTitle
+        TransationSelect = req.body.TransationSelect
+        res.send(TransationSelect)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+app.post("/register", async (req, res) => {
+    if (!req.body.Name || !req.body.EmailAddress || !req.body.PhoneNumber || !req.body.Password || !req.body.ConfirmPassword ) {
+        return res.status(422).json({ error: "Please Filled The Field Properly!!!" })
+    }
+
+    try {
+        const userExist = await User.findOne({ EmailAddress: req.body.EmailAddress });
+        if (userExist) {
+            // return req.status(422).json({ error: "Email ALready Exists!!!" })
+            res.send("Email ALready Exists!!!")
+        } else if (req.body.Password != req.body.ConfirmPassword) {
+            // return req.status(422).json({ error: "Password doesn;t match with each other!!!" })
+            res.send("Password doesn;t match with each other!!!!")
+        } else {
+            const user = new User(req.body);
+            await user.save();
+            // return req.status(201).json({ error: "User Registered Successfully!!!" })
+            res.send("User Registered Successfully!!!")
+        }
+    } catch (error) {
+        res.send("Data Nhi aaya")
+    }
+})
+
+
+app.post("/login", async (req, res) => {
+    try {
+        LoginEmail = req.body.LoginEmail
+        LoginPassword = req.body.LoginPassword
+        res.send("Data AA gya")
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+app.listen(port, () => {
+    console.log(`connection is setup at ${port}`);
+})
+
--- src/db/conn.js
+++ src/db/conn.js
@@ -1,11 +1,18 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/students-api", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true
-}).then(() => {
-    console.log("Connection Successfully");
-}).catch((e) => {
-    console.log("No Connection");
+// mongoose.connect("mongodb://localhost:27017/students-api", {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//     useCreateIndex: true
+// }).then(() => {
+//     console.log("Connection Successfully");
+// }).catch((e) => {
+//     console.log("No Connection");
+// })
+
+mongoose.connect("mongodb://localhost/walletBalance")
+mongoose.connection.once('open', function(){
+console.log("Connection Successfully");
+}).on('error', function(error){
+    console.log("No Connection", error);
 })
